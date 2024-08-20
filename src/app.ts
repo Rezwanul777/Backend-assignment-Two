716b15d016f61022954b1bc8c83d8ec9de5cf70a
Rezwanul777 <rezwanul.haquebd777@gmail.com>
2024-08-21 00:36:15 +0600

    some utility setup and error validation setup

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
-import express, { Request, Response } from 'express';
-const app = express()
+import express, {Application, Request, Response } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './middlewares/globalErrorHandelar';
+import notFound from './middlewares/notFound';
+
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
 
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
+
+app.use(globalErrorHandler);
+
+//Not Found
+app.use(notFound);
 
 
 export default app;
